980e9bb3a7a071cf5de3796bd2db3fdd769066c6
elincauto <victormcnz@gmail.com>
2019-08-14 00:06:35 +0200

    seguimos

--- app/views.py
+++ app/views.py
@@ -1,5 +1,11 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 def index():
-    return 'Hola,mundo'
+    return render_template('index.html')
+   
+
+@app.route('/adios')
+def second_page():
+    return render_template('adios.html')
