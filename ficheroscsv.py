429a3a19e16756fb56f175d9bc5d7492ff68f4e2
elincauto <victormcnz@gmail.com>
2019-08-14 22:50:23 +0200

    seguimos

---
+++ ficheroscsv.py
@@ -0,0 +1,20 @@
+import csv
+
+fMedias=open('data/medias.txt','w')
+fAlumnos=open('data/alumnos.txt', 'r')
+csvreader =csv.reader(fAlumnos,delimiter=',')
+
+for campos in csvreader:
+    
+    notas_alumno= campos[2:]
+
+    suma=0
+
+    for nota in notas_alumno:
+        suma +=float(nota)
+    media=suma/len(notas_alumno)
+    fMedias.write('{},{},{}\n'.format(campos[0],campos[1],media))
+
+fAlumnos.close()   
+fMedias.close()
+
