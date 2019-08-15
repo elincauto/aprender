0dc5e5a4c48e02d1eb9ac3ea26a391a0c2976e13
elincauto <victormcnz@gmail.com>
2019-08-15 23:28:08 +0200

    seguimos

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template,request,redirect,url_for,flash
 import csv
 
 ficheromovimientos='data/movimientos.txt'
@@ -19,6 +19,44 @@ def index():
     return render_template('index.html',movimientos=movimientos)
    
 
-@app.route('/nuevacompra')
+@app.route('/nuevacompra', methods=['GET','POST'])
 def compra():
-    return render_template('adios.html')
+    print(request.method)
+    if request.method=='GET':       
+        return render_template('nuevacompra.html')
+
+    else:
+        msg=validar(request.values)  != True
+        if msg!=True
+            flash(msg)
+            return render_template('nuevacompra.html')
+       
+        fMovimientos=open(ficheromovimientos,'a+')
+        precioUnitario=request.values['cantidadPagada']/request.values['cantidadComprada']  
+        registro='{},'{}',{},{},{},{},{}\n'.format(request.values['fecha'],
+        request.values['concepto'],request.values['monedaComprada'],
+        request.values['cantidadComprada'],
+        request.values['monedaPagada'],
+        request.values['cantidadPagada'],precioUnitario)
+        fMovimientos.write(registro)
+        fMovimientos.close()
+        return redirect(url_for('index'))
+    
+    def validar(values):
+        if values['fecha']=='':
+            return 'Debe informar la fecha'
+
+        if values['concepto']=='':
+            return 'Debe informar el concepto'
+
+         if values['cantidadComprada']=='':
+            return 'Debe informar una cantidad mayor que cero para cantidad comprada'
+
+        if values['cantidadPagada']=='':
+            return 'Debe informar un valor mayor que cero'
+
+        return True
+
+
+
+
