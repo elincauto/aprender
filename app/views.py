9f1b399c5e112c2e74429b9f0825b4066d54af5a
elincauto <victormcnz@gmail.com>
2019-08-15 07:12:45 +0200

    seguimos

--- app/views.py
+++ app/views.py
@@ -1,11 +1,24 @@
 from app import app
 from flask import render_template
+import csv
+
+ficheromovimientos='data/movimientos.txt'
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    #leer movimientos
+    fMovimientos= open(ficheromovimientos,'r')
+    csvreader=csv.reader(fMovimientos, delimiter=',' , quotechar='"')
+
+    movimientos =[]
+    for movimiento in csvreader:
+        movimientos.append(movimiento) 
+
+        
+    #enviar movimientos
+    return render_template('index.html',movimientos=movimientos)
    
 
-@app.route('/adios')
-def second_page():
+@app.route('/nuevacompra')
+def compra():
     return render_template('adios.html')
